cdd06cbff3c711498923b0b3bb299ed7c6ec4edf
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-10 17:56:17 +0600

    declare 'snake case' , 'kebab case', 'camel case', 'pascal case'

--- string_to_ucwords.php
+++ string_to_ucwords.php
@@ -1,3 +1,8 @@
 <?php
  $message = "hello from noakhali";
  echo ucwords($message);
+
+//  snake case = hello_from_noakhali
+// kebab case = hello-from-noakhali
+// camel case = helloFromNoakhali
+// pascal case = HelloFromNoakhali
