c9dfb83394f2f554396e19a54f7b3d842d5f980d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-13 10:40:54 +0600

    Declare String Word Wrap

---
+++ string_wordwrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ String word wrapping is the process of breaking a long string of text into multiple
+ lines to fit within a specific width. It's essential for improving readability and preventing
+ text from over flowing its container.
+
+*/
+$message = "Lorem ipsum dolor sit amet eeeeeeeconsectetur adipisicing elit. Eligendi tenetur nam adipisci eum pariatur expedita veritatis amet vitae. Asperiores possimus blanditiis autem sapiente ex. Possimus iste cum explicabo deleniti ut.";
+echo wordwrap($message,20,"\n", false);
